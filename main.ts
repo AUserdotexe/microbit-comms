1b43ff4d30d2de189287919eeb87f879ce3c8442
AUserdotexe <mboteraradillos@gmail.com>
2026-03-16 19:07:26 +0800

    Added sounds. Don't worry, they're totally not audible to your teacher!

--- main.ts
+++ main.ts
@@ -1,30 +1,48 @@
 input.onButtonPressed(Button.A, function () {
     basic.clearScreen()
+    music.play(music.tonePlayable(523, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
     message = "" + message + "."
-    basic.showString(".")
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
 })
 input.onButtonPressed(Button.AB, function () {
     message = ""
     basic.clearScreen()
+    music.play(music.tonePlayable(392, music.beat(BeatFraction.Double)), music.PlaybackMode.UntilDone)
     // Optional: Visual cue that it's cleared
     basic.showIcon(IconNames.No)
     basic.pause(500)
     basic.clearScreen()
 })
 radio.onReceivedString(function (receivedString) {
+    music.play(music.stringPlayable("C5 B A B A B C5 C5 ", 120), music.PlaybackMode.UntilDone)
     basic.showString(receivedString)
 })
 input.onButtonPressed(Button.B, function () {
     basic.clearScreen()
+    music.play(music.tonePlayable(587, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
     message = "" + message + "-"
-    basic.showString("-")
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . # # # .
+        . . . . .
+        . . . . .
+        `)
 })
 input.onLogoEvent(TouchButtonEvent.Touched, function () {
     radio.sendString(message)
     basic.showIcon(IconNames.Yes)
     message = ""
+    basic.pause(500)
     basic.clearScreen()
 })
 let message = ""
+music.play(music.tonePlayable(880, music.beat(BeatFraction.Double)), music.PlaybackMode.UntilDone)
 radio.setGroup(1)
 message = ""
